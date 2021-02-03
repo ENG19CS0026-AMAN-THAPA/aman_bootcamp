ddeb0976d822a78a14d785c54196be4725790157
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-03 20:09:09 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,8 @@
 //Write the program to print "Hello World !"
 #include<stdio.h>
-void main()
+int main()
 {
-printf("My name is Aman Thapa. \n");
+  printf("My name is Aman Thapa. \n");
+  return 0;
 }
 //no changes
