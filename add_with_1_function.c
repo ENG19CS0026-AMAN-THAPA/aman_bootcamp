2569933122f3a4786ebd763a5dd556444c43c7e2
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-03 20:33:46 +0530

    initial commit

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,1 +1,12 @@
 //Write a program to add two user input numbers using one function.
+
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("Enter two numbers: \n");
+	scanf("%d %d", &a, &b);
+	c = a + b;
+	printf("Sum of two numbers is: ", c);
+	return 0;
+}
