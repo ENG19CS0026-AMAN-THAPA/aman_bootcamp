1649f747549e832562a08f1983553c2fa5eaf8a7
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-04 11:09:52 +0530

    initial commit

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,1 +1,12 @@
 //Write a program to find the volume of a tromboloid using one function
+
+#include<stdio.h>
+int main()
+{
+	float vol, h, d, b;
+	printf("Input the values of h, d, b respectively: \n");
+	scanf("%f%f%f", &h, &d, &b);
+	vol = 1/3((h*b*d) + (d/b))
+	printf("Volume of the tromboloid is: %f", vol);
+	return 0;
+}
