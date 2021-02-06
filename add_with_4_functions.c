b62f8f84ef105c2cc950c086309fcf5bf037bf0c
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-06 10:38:59 +0530

    initial commit

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,1 +1,16 @@
 //Write a program to add two user input numbers using 4 functions.
+
+#include<stdio.h>
+int sum(int, int)
+int main()
+{
+	int a, b;
+	printf("Enter two numbers: \n");
+	scanf("%d%d", &a, &b);
+	printf("Sum of the given numbers is: %d\n", sum(a, b));
+	return 0;
+}
+int sum(int x, int y)
+{
+	return(x+y);
+}
