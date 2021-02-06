21e0c867e753c94b0a1da606e51f33fcd8d3f9d6
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-06 11:12:01 +0530

    initial commit

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,16 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+
+#include<stdio.h>
+float vol(float, float, float);
+int main()
+{
+	float h, d. b;
+	printf("Enter the values of h, d, b respectively: \n");
+	scanf("%f%f%f", &h, &d, &b);
+	printf("Volume of tromboloid is: %f", vol(h, d, b));
+	return 0;
+}
+float vol(float h, float d, float b)
+{
+	return(((h*d*b) + (d/b)) * 1/3);
+}
