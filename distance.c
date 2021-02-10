2f8fe6064967019c4bc2c23263b46846ed153f47
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-10 10:59:04 +0530

    second commit

--- distance.c
+++ distance.c
@@ -4,30 +4,31 @@
 #include<math.h>
 float input()
 {
-	float a;
-	scanf("%f", &a);
-	return a;
+    float a;
+    scanf("%f", &a);
+    return a;
 }
-float distance(float x1, float x2, float y1, float y2)
+float distance(float x1, float x2, float y1, float y2, float d)
 {
-	return(sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    d = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return(sqrt(d));
 }
 float show(float dis)
 {
-	printf("Distance between the points is: %f", dis);
+    printf("Distance between the points is: %0.4f", dis);
 }
 int main()
 {
-	float n1, n2, n3, n4, result;
-	printf("Enter the value of x1: ");
-	n1 = input();
-	printf("Enter the value of x2: ");
-	n2 = input();
-	printf("Enter the value of y1: ");
-	n3 = input();
-	printf("Enter the value of y2: ");
-	n4 = input();
-	result = distance(n1, n2, n3, n4, result);
-	show(result);
-	return 0;
+    float n1, n2, n3, n4, result;
+    printf("Enter the value of x1: ");
+    n1 = input();
+    printf("Enter the value of x2: ");
+    n2 = input();
+    printf("Enter the value of y1: ");
+    n3 = input();
+    printf("Enter the value of y2: ");
+    n4 = input();
+    result = distance(n1, n2, n3, n4, result);
+    show(result);
+    return 0;
 }
