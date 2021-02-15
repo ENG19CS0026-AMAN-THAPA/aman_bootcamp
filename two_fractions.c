f0cb2263ee35e82d43302f333e680fba4870d60a
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-02-15 21:50:18 +0530

    initial commit

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,39 @@
 //WAP to find the sum of two fractions.
+
+#include<stdio.h>
+struct fract
+{
+    int num, deno, x, y;
+};
+int input1()
+{
+    printf("Enter numerator of fraction 1: ");
+    scanf("%d", &x.num);
+    printf("Enter the denominator of fraction 1: ");
+    scanf("%d", &x.deno);
+}
+int input2()
+{
+    printf("Enter numerator of fraction 2: ");
+    scanf("%d", &x.num);
+    printf("Enter denominator of fraction 2: ");
+    scanf("%d", &x.deno);
+}
+int gcd_calc()
+{
+    int gcd, a, b, i;
+    a = (x.num*y.deno) + (y.num*x.deno);
+    b = x.deno*y.deno;
+    for(i=1; i<=a && i<=b; i++)
+    {
+        if(a%i==0 && b%i == 0)
+        gcd = i;
+    }
+    printf("The added fraction is %d/%d", a/gcd, b/gcd); 
+}
+int main()
+{
+    input1();
+    input2();
+    gcd_calc();
+}
