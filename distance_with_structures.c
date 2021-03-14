fa8566c51685c4dd2abe63cc376328725692ee0d
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-03-14 22:40:12 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,34 +1,49 @@
 //WAP to find the distance between two points using structures and 4 functions.
 
 #include<stdio.h>
-#include<math.h>
-struct points
+void disp(int, int);
+struct fract
+{ 	
+int num, denom;
+};
+typedef struct fract fr;
+fr input()
 {
-	int x,y;
-}p1, p2;
-
-void input()
-{
-printf("Enter the coordinates for point 1: ");
-	scanf("%d%d",&p1.x, &p1.y);
-	printf("Enter the coordinates for point 2: ");
-	scanf("%d%d", &p2.x,&p2.y);
-	printf("%d %d %d %d\n", p1.x,p1.y, p2.x, p2.y);
+	fr x;
+	printf("Enter numerator of the fraction: ");
+	scanf("%d", &x.num);
+	printf("Enter denominator of the fraction: ");
+	scanf("%d", &x.denom);
+	printf("The entered fraction is: %d/%d\n", x.num, x.denom);
+	return (x);
 }
 
-void display(float d)
+int sumf(int a, int b, int c, int d)
 {
-	printf("The distance between (%d,%d) and (%d,%d) is %f units\n", p1.x, p1.y, p2.x, p2.y,d);
+int i, x, y, gcd, sum;
+x = (a*d) + (c*b);
+y = b*d;
+	for(int i=1; i<=x && i<=y; i++)
+	{
+		if((x%i==0)&&(y%i == 0))
+			gcd = i;
+	}
+	x = x/gcd; y = y/gcd;
+	disp(x,y);
+}
+void disp(int x, int y)
+{
+    printf("The sum of entered fractions is %d/%d",x,y);
 }
 
 int main()
 {
-	float dis;
-	int xco, yco;
-	input();
-	xco = pow((p1.x - p2.x),2);
-	yco = pow((p1.y- p2.y),2);
-	dis = sqrt(xco+yco);
-	display(distance);
+	fr o,t;
+	int sum;
+	printf("FRACTION 1 -\n");
+    	o = input();
+   	printf("FRACTION 2 -\n");
+	t = input();
+	sum = sumf(o.num, o.denom, t.num, t.denom);
 	return 0;
 }
