77e640c0b84b4e5a84dc4dfc884af4a30135c9df
ENG19CS0026-AMAN-THAPA <76113627+ENG19CS0026-AMAN-THAPA@users.noreply.github.com>
2021-03-14 22:35:15 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -2,44 +2,33 @@
 
 #include<stdio.h>
 #include<math.h>
-struct Points
+struct points
 {
-	float i;
-};
-float input()
+	int x,y;
+}p1, p2;
+
+void input()
 {
-    struct Points a;
-    scanf("%f", &a.i);
-    return a.i;
-}
-float distance(float x1, float x2, float y1, float y2)
-{
-    float x, y, d;
-    x = pow((x1 - x2), 2);
-    y = pow((y1 - y2), 2);
-    d = x + y;
-	return(sqrt(d));
-}
-void show(float dis)
-{
-    printf("Distance between the points is: %0.4f", dis);
-}
-int main()
-{
-	
-    float x1, x2, y1, y2, result;
-	printf("\nEnter coordinates of first point: \n");
-	printf("Enter the value of x1: ");
-	x1 = input();
-	printf("Enter the value of y1: ");
-	y1 = input();
-	printf("\nEnter coordinates of second point: \n");
-	printf("Enter the value of x2: ");
-	x2 = input();
-	printf("Enter the value of y2: ");
-	y2 = input();
-    result = distance(x1, x2, y1, y2);
-    show(result);
-	return 0;
+printf("Enter the coordinates for point 1: ");
+	scanf("%d%d",&p1.x, &p1.y);
+	printf("Enter the coordinates for point 2: ");
+	scanf("%d%d", &p2.x,&p2.y);
+	printf("%d %d %d %d\n", p1.x,p1.y, p2.x, p2.y);
 }
 
+void display(float d)
+{
+	printf("The distance between (%d,%d) and (%d,%d) is %f units\n", p1.x, p1.y, p2.x, p2.y,d);
+}
+
+int main()
+{
+	float dis;
+	int xco, yco;
+	input();
+	xco = pow((p1.x - p2.x),2);
+	yco = pow((p1.y- p2.y),2);
+	dis = sqrt(xco+yco);
+	display(distance);
+	return 0;
+}
